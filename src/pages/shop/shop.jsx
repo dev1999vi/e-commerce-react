aa12728a488944f1ff9fa452ddf27a14bb048b77
Devendra Yadav <dev1999.2011vi@gmail.com>
2023-03-20 12:14:19 +0530

    cart and shop component added

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Shop = () => {
+  return (
+    <div className='shop'>
+        <div className='shopTitle'>
+            <h1>Bad Items Shop</h1>
+        </div>
+        <div className='products'>
+            
+        </div>
+    </div>
+  )
+}
