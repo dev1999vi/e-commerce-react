81885e3b5959b86ec89ea55bfa49f7f5e9dd1417
Devendra Yadav <dev1999.2011vi@gmail.com>
2023-03-20 12:04:05 +0530

    navbar added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'phosphor-react';
+import './navbar.css';
 
 export const Navbar = () => {
   return (
-    <div></div>
+    <nav className='navbar'>
+      <div className='links'>
+        <Link to='/'>Shop</Link>
+        <Link to='/cart'><ShoppingCart size={32}/></Link>
+      </div>
+    </nav>
   )
 }
