24f2fa6d5777ee7137cb67f440074ca3d5bdfd7e
Devendra Yadav <dev1999.2011vi@gmail.com>
2023-03-20 11:40:57 +0530

    Intial Comimit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Navbar } from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' />
+          <Route path='/cart' />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
